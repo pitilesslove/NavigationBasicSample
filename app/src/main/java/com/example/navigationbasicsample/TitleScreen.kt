c1ce0193f99767858373c2bfb253ece3f9bbe680
mhchoi <mh.choi@altimedia.com>
2022-09-13 16:57:22 +0900

    feat: TitleScreen 프레그먼트 추가
    TitleScreen 프레그먼트 추가

---
+++ app/src/main/java/com/example/navigationbasicsample/TitleScreen.kt
@@ -0,0 +1,27 @@
+package com.example.navigationbasicsample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.navigationbasicsample.databinding.FragmentTitleScreenBinding
+
+class TitleScreen : Fragment() {
+
+    private lateinit var binding: FragmentTitleScreenBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentTitleScreenBinding.inflate(inflater)
+
+        binding.playBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_title_screen_to_FirstFragment)
+        }
+        binding.leaderboardBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_title_screen_to_SecondFragment)
+        }
+
+        return binding.root
+    }
+}
